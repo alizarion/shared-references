d29f83c9b60bc789b7825772c2bf867cd7649e66
alizarion <selim@openlinux.fr>
2014-09-23 18:23:34 +0200

    security : update unit test.

--- security/src/test/java/com/alizarion/reference/security/CredentialTest.java
+++ security/src/test/java/com/alizarion/reference/security/CredentialTest.java
@@ -1,7 +1,9 @@
 package com.alizarion.reference.security;
 import com.alizarion.reference.security.entities.Credential;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 
 public class CredentialTest {
 
@@ -15,38 +17,11 @@ public class CredentialTest {
 
     @Test
     public void setCredentialSha1Password(){
-        //TODO corriger le test
-        Credential newCredential = new Credential();
-        newCredential.setPassword("TOTO");
-       // assertEquals(newCredential.getPassword(),credential.getPassword());
-       TesTThread  tesTThread1  = new TesTThread(1);
-        TesTThread  tesTThread2  = new TesTThread(2);
-        TesTThread  tesTThread3  = new TesTThread(3);
+
+        Assert.assertTrue(credential.
+                isCorrectPassword("TOTO"));
 
 
-        new Thread(tesTThread1).start();
-        new Thread(tesTThread2).start();
-        new Thread(tesTThread3).start();
-
-    }
-
-    public class TesTThread implements Runnable {
-
-        private  int threadNumber;
-
-        public TesTThread( int threadNumber) {
-            super();
-            this.threadNumber = threadNumber;
-        }
-
-        public synchronized void run(){
-              System.out.println("Thread numero" + threadNumber);
-            try {
-              Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 }
